5ddc840b28cf9365834d6f79590054f1ce63e651
Christian Bahnweg <cebahnweg@gmail.com>
2019-07-29 21:50:53 -0400

    sets up scaffolding for 2.7

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -2,23 +2,48 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', key: 1 }
   ]) 
   const [ newName, setNewName ] = useState('')
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    setPersons(() => {
+      let entry = {
+        name: `${newName}`,
+        key: persons.length + 1
+      }
+      let people = [...persons];
+      const results = people.filter((person) => {
+        return person == entry
+      })
+      if(results.length !== 0) {
+        people.push(entry)
+      }
+      else alert("Already exists!")
+      return people
+    })
+  }
+  const addName = (event) => {
+    setNewName(event.target.value)
+    console.log(event.target.value)
+  }
+
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNumber}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={addName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => <p key={person.key}>{person.name}</p>)}
     </div>
   )
 }
